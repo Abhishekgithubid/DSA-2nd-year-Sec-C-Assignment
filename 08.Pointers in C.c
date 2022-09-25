96152e1fb334ee05a21e77d04d5a7e4f3db50c6a
Abhishekgithubid <113165789+Abhishekgithubid@users.noreply.github.com>
2022-09-25 16:51:21 +0530

    Add files via upload

---
+++ 08.Pointers in C.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+void update(int *a,int *b) {
+    // Complete this function  
+    // temporary int
+    int c=*a;
+    *a = c+*b;
+    *b = abs(*b-c);  
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
