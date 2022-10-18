3d0539cc2655331e0bdbffebbbedf14343a2b8b7
Abhishekgithubid <113165789+Abhishekgithubid@users.noreply.github.com>
2022-10-18 21:35:09 +0530

    Create delete node at end.c

---
+++ delete node at end.c
@@ -0,0 +1,15 @@
+SinglyLinkedListNode* deleteNode(SinglyLinkedListNode* llist, int position) {
+
+ if((position) == 0) {
+
+        return llist->next;
+
+    }
+
+    llist->next = deleteNode(llist->next, position-1);
+
+    return llist;
+
+    
+
+}
